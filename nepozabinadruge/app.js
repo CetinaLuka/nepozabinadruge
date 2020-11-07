5fc7f6e0aaac9c5730fd220f3f3001ddf05dd8c7
tilen <tilen.ravnak@studnet.um.si>
2020-11-07 22:18:26 +0100

    Changes

--- nepozabinadruge/app.js
+++ nepozabinadruge/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/userRoutes');
 var botRouter = require('./routes/botRoutes');
+var requestRouter = require('./routes/requestRoutes');
 
 var app = express();
 
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bot', botRouter);
+app.use('/request', requestRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
