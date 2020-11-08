c497274e6e92c6cea4b831cf4a0ef266f67a3ef1
David Zele <david.zele@etrust.si>
2020-11-08 05:09:01 +0100

    Added minigame

--- nepozabinadruge/app.js
+++ nepozabinadruge/app.js
@@ -28,6 +28,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/userRoutes');
 var botRouter = require('./routes/botRoutes');
 var requestRouter = require('./routes/requestRoutes');
+var minigamesRouter = require('./routes/minigameRoutes');
 
 var app = express();
 
@@ -45,6 +46,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bot', botRouter);
 app.use('/request', requestRouter);
+app.use('/minigames', minigamesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
